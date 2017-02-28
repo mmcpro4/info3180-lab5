eca40af57e74f50890d4115bb36c5352b937f3eb
mmcpro4 <chevaughn_barnett@yahoo.com>
2017-02-28 03:44:12 +0000

    migration folder added

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
+app.config['SECRET_KEY'] = "a more random key"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:info3180@localhost/database"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
